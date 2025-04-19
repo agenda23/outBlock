8f4e5f48405d4c2aadb5dd8d57d614ba74748dbf
masataka koguchi <agenda23@gmail.com>
2025-04-19 11:27:19 +0900

    edit files

--- background/background.js
+++ background/background.js
@@ -31,22 +31,19 @@ function initialize() {
 
 // アイコン状態の更新
 function updateIcon(enabled) {
-  // 無効化時も通常のアイコンを使用し、別の方法で無効状態を表現する
-  const iconPath = { 
-    16: 'icons/icon16.png',
-    48: 'icons/icon48.png',
-    128: 'icons/icon128.png'
-  };
+  const iconPath = enabled 
+    ? { 
+        16: 'icons/icon16.png',
+        48: 'icons/icon48.png',
+        128: 'icons/icon128.png'
+      }
+    : {
+        16: 'icons/icon16_disabled.png',
+        48: 'icons/icon48_disabled.png',
+        128: 'icons/icon128_disabled.png'
+      };
   
   chrome.action.setIcon({ path: iconPath });
-  
-  // 無効状態の場合は、アイコンをグレースケールや透明度で表現する
-  if (!enabled) {
-    chrome.action.setBadgeText({ text: 'OFF' });
-    chrome.action.setBadgeBackgroundColor({ color: '#888888' });
-  } else {
-    chrome.action.setBadgeText({ text: '' });
-  }
 }
 
 // Webリクエストフィルタの設定
@@ -174,18 +171,6 @@ function toggleEnabled(enabled) {
   currentSettings.enabled = enabled;
   chrome.storage.sync.set({ enabled: enabled });
   updateIcon(enabled);
-  
-  // 全てのタブのバッジを更新
-  chrome.tabs.query({}, function(tabs) {
-    tabs.forEach(function(tab) {
-      if (enabled) {
-        // 有効化された場合、ブロックカウントを表示
-        updateBadge(tab.id);
-      } else {
-        // 無効化されている場合は、updateIconで設定されるのでここでは何もしない
-      }
-    });
-  });
 }
 
 // サイト固有の設定更新
@@ -222,19 +207,15 @@ function getSiteSettings(url, sendResponse) {
 
 // バッジの更新
 function updateBadge(tabId) {
-  // 拡張機能が有効な場合のみブロックカウントを表示
-  if (currentSettings.enabled) {
-    chrome.action.setBadgeText({
-      text: currentSettings.blockedCount.toString(),
-      tabId: tabId
-    });
-    
-    chrome.action.setBadgeBackgroundColor({
-      color: '#E03131',
-      tabId: tabId
-    });
-  }
-  // 無効の場合は、updateIcon関数でバッジが設定される
+  chrome.action.setBadgeText({
+    text: currentSettings.blockedCount.toString(),
+    tabId: tabId
+  });
+  
+  chrome.action.setBadgeBackgroundColor({
+    color: '#E03131',
+    tabId: tabId
+  });
 }
 
 // エラー処理
